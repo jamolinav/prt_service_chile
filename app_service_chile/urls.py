85d54faabbd983be06a5c8b4a5d6eb51ee884de8
Juan Molina <juanmolina@NB-JMOLINA.local>
2021-09-25 19:07:10 -0300

    update

--- app_service_chile/urls.py
+++ app_service_chile/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('home/', views.index, name='home'),
+    path('home', views.index, name='home'),
+    path('gps', views.gps, name='gps'),
+    path('geolocation', views.geolocation, name='geolocation'),
 ]
--- app_service_chile/views.py
+++ app_service_chile/views.py
@@ -18,13 +18,14 @@ def index(request):
 
     # Create Map Object
     #m = folium.Map(location=[-33.462549, -70.655102], zoom_start=16)
-    m = folium.Map(location=[-33.46222, -70.65518], zoom_start=16)
+    m = folium.Map(location=[-33.46011033534138, -70.65566059512977], zoom_start=16)
 
-    icon_prt = folium.features.CustomIcon('http://127.0.0.1:5500/MyProjects/web-locator/prt.jpg', icon_size=(50, 50))
-    icon_porche_naranjo = folium.features.CustomIcon('http://127.0.0.1:5500/MyProjects/web-locator/porche_rojo.png', icon_size=(50, 50))
-    folium.Marker([-33.46011033534138, -70.65566059512977], tooltip='Click for more', icon=icon_prt, popup=country).add_to(m)
+    icon_prt = folium.features.CustomIcon('app_service_chile/static/imgs/prt.jpg', icon_size=(50, 50))
+    icon_porche_naranjo = folium.features.CustomIcon('app_service_chile/static/imgs/porche_rojo.png', icon_size=(50, 50))
+    #folium.Marker([-33.46011033534138, -70.65566059512977], tooltip='Click for more', icon=icon_prt, popup=country).add_to(m)
 
-    folium.Marker([-33.46254631098106, -70.65507286554855], tooltip='Click for more', icon=icon_porche_naranjo, popup=country).add_to(m)
+    folium.Marker([-33.46011033534138, -70.65566059512977], tooltip='Click for more', icon=icon_porche_naranjo, popup=country).add_to(m)
+    #folium.Marker([-33.46254631098106, -70.65507286554855], tooltip='Click for more', icon=icon_porche_naranjo, popup=country).add_to(m)
     # Get HTML Representation of Map Object
     m = m._repr_html_()
     context = {
@@ -32,3 +33,29 @@ def index(request):
     }
 
     return render(request, 'index.html', context)
+
+def gps(request):
+    print('GPS post')
+    print(request.POST)
+
+    lat = request.POST['latitude']
+    lng =  request.POST['longitude']
+
+    lat = -36.6330069
+    lng = -72.9563881
+
+    m = folium.Map(location=[lat, lng], zoom_start=16)
+
+    icon_porche_naranjo = folium.features.CustomIcon('app_service_chile/static/imgs/porche_rojo.png', icon_size=(50, 50))
+
+    folium.Marker([lat, lng], tooltip='Click for more', icon=icon_porche_naranjo).add_to(m)
+    m = m._repr_html_()
+
+    context = {
+        'm': m,
+    }
+    print('render')
+    return render(request, 'index.html', context)
+
+def geolocation(request):
+    return render(request, 'leaflet-locatecontrol/demo/index.html')
